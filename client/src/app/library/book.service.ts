5f49a1c44cdd31420b403827f4ee37b4ecc18944
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2024-06-06 14:03:25 +0300

    installed angular fire

--- client/src/app/library/book.service.ts
+++ client/src/app/library/book.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 
 import { Book } from './models/book.model';
 import { environment } from '../../environments/environment.development';
