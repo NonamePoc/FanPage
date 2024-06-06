19c97e0f1e699c059176165285ff6be8120f4770
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2024-06-06 14:30:43 +0300

    Deleted angular/fire, installed dotenv
    In process of trying to deploy app (having a problem with environment variables
    Created node.js file outside of src
    Added .env (git ignored)

---
+++ client/node.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+const successColor = "\x1b[32m%s\x1b[0m";
+const checkSign = "\u{2705}";
+const dotenv = require("dotenv").config({ path: "src/.env" });
+
+const envFile = `export const environment = {
+  apiUrl: '',
+  editorApiKey: '',
+  googleClientId: '',
+  recaptcha: {
+    siteKey: '',
+    secretKey: '',
+  },
+};
+`;
+const targetPath = path.join(
+  __dirname,
+  "./src/environments/environment.development.ts"
+);
+fs.writeFile(targetPath, envFile, (err) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(
+      successColor,
+      `${checkSign} Successfully generated environment.development.ts`
+    );
+  }
+});
